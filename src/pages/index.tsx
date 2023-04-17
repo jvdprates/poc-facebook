6e9c8cf57dde4210155b1d5268bf395238858c80
João Prates <jvdprates@hotmail.com>
2023-04-17 15:52:05 -0300

    feat: Testing greatsumini

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,16 @@
 import { Box } from "@mui/material";
 import { NextPage } from "next";
+import FacebookLogin, { FailResponse, SuccessResponse } from '@greatsumini/react-facebook-login';
+
+const onSuccess = (success: SuccessResponse) => {
+  console.log("🚀 ~ file: index.tsx:7 ~ successFacebook ~ success:", success)
+}
+
+const onFail = (fail: FailResponse) => {
+  console.log("🚀 ~ file: index.tsx:11 ~ onClick ~ fail:", fail)
+}
+
+const facebookAppId = process.env.FACEBOOK_APP_ID ?? '';
 
 const Index: NextPage = () => (
   <Box
@@ -8,7 +19,12 @@ const Index: NextPage = () => (
     alignItems="center"
     justifyContent="center">
     <Box>
-    Under construction
+      <FacebookLogin
+        appId={facebookAppId}
+        fields="name,email,picture"
+        onSuccess={onSuccess}
+        onFail={onFail}
+      />
     </Box>
   </Box>
 );
