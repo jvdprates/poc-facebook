0cf06898e1a9d48c9c5d662f7ea555edf8fd6956
João Prates <jvdprates@hotmail.com>
2023-04-17 17:54:02 -0300

    fix: Eslint config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
-import { Box } from "@mui/material";
-import { NextPage } from "next";
-import FacebookLogin, { FailResponse, SuccessResponse } from '@greatsumini/react-facebook-login';
+import {Box} from '@mui/material';
+import {NextPage} from 'next';
+import FacebookLogin from 'react-facebook-login';
+import GFacebookLogin, {
+  FailResponse,
+  SuccessResponse,
+} from '@greatsumini/react-facebook-login';
+import {MouseEvent} from 'react';
 
 const onSuccess = (success: SuccessResponse) => {
-  console.log("🚀 ~ file: index.tsx:7 ~ successFacebook ~ success:", success)
-}
+  console.log('🚀 ~ file: index.tsx:7 ~ successFacebook ~ success:', success);
+};
 
 const onFail = (fail: FailResponse) => {
-  console.log("🚀 ~ file: index.tsx:11 ~ onClick ~ fail:", fail)
-}
+  console.log('🚀 ~ file: index.tsx:11 ~ onClick ~ fail:', fail);
+};
+
+const onClick = (e: MouseEvent<HTMLDivElement>) => {
+  console.log('🚀 ~ Click', e.detail);
+};
+
+const onCallback = (response: FailResponse) => {
+  console.log('🚀 ~ file: index.tsx:20 ~ onCallback ~ response:', response);
+};
 
 const facebookAppId = process.env.FACEBOOK_APP_ID ?? '';
 
@@ -18,12 +31,18 @@ const Index: NextPage = () => (
     height="100vh"
     alignItems="center"
     justifyContent="center">
-    <Box>
-      <FacebookLogin
+    <Box display="flex" flexDirection="column" gap="16px">
+      <GFacebookLogin
         appId={facebookAppId}
         fields="name,email,picture"
         onSuccess={onSuccess}
         onFail={onFail}
+      />
+      <FacebookLogin
+        appId={facebookAppId}
+        fields="name,email,picture"
+        onClick={onClick}
+        callback={onCallback}
       />
     </Box>
   </Box>
